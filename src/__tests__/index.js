b609a3b1bb5ce4aa67a79642645df47431d82831
Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>
2021-05-10 11:47:15 +0100

    #14 Adds accessibility test using axe
    Co-authored-by: Yusra Dahir (TIN TULIP) <yusra.dahir+tintulip@thoughtworks.com>

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,10 +1,17 @@
-import React from "react"
-import { renderer } from "react-test-renderer"
-import Index from "../pages/index"
+import React from 'react';
+import { render } from '@testing-library/react';
+import Index from '../pages/index';
+import { axe, toHaveNoViolations } from 'jest-axe';
 
+expect.extend(toHaveNoViolations);
 
-describe("Index", () => {
-  it("renders correctly", () => {
-    expect(true).toBe(true)
-  })
-})
+describe('Index', () => {
+  it('renders correctly', () => {
+    expect(true).toBe(true);
+  });
+  it('should not have any accessibility violations', async () => {
+    const { container } = render(<Index />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
